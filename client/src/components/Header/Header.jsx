a10f37a1ad4b04cc17df5aef6bb360cada03d994
shreyvegad <shreyvegad22@gmail.com>
2022-09-17 19:13:46 +0530

    Added Nav,Login form

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../Header/Header.module.css'
+// import gujgov from '../../assets/guj-gov.png'
+const Header = () => {
+  return (
+    <div className={styles.container}>
+          <div className={styles.heading}>
+              <img src=''></img>
+          </div>
+          
+    </div>
+  )
+}
+
+export default Header;
