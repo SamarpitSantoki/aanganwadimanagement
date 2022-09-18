4d1a33aaf16f9471e2ca33870dd4ff72dd251d0b
shreyvegad <shreyvegad22@gmail.com>
2022-09-18 21:56:10 +0530

    Add User List

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,18 +1,16 @@
 import "../App.css";
 import Header from "../components/Header/Header";
 import Nav from "../components/Header/Nav";
-import Login from "../components/LoginForm/LoginForm";
+import UserList from "../components/UserList/UserList";
 
 function HomePage() {
-  //   const [count, setCount] = useState(0)
+    //   const [count, setCount] = useState(0)
 
-  return (
-    <div>
-      <Nav />
-      <Header />
-      <Login />
-    </div>
-  );
+    return (
+        <div className="container mt-5">
+            <UserList />
+        </div>
+    );
 }
 
 export default HomePage;
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,12 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet,Route,Routes } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Nav from "../components/Header/Nav";
 import HomePage from "../pages/HomePage";
+import Login from "../pages/Login";
 function index() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Nav />
+            <Header />
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<HomePage />} />
+            </Routes>
+        </div>
+    );
 }
 export default index;
