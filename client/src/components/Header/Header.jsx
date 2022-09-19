ed8a4c635773ff70352cb5055c0b6a35704ef632
shubham <shubhamrupapara993@gmail.com>
2022-09-19 21:52:34 +0530

    nav

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import styles from '../Header/Header.module.css'
+import React from "react";
+import styles from "../Header/Header.module.css";
+import style from "../Header/Nav.module.css";
+
 // import gujgov from '../../assets/guj-gov.png'
 const Header = () => {
   return (
-    <div className={styles.container}>
-          <div className={styles.heading}>
-              <img src=''></img>
-          </div>
-          
-    </div>
-  )
-}
+    <>
+      <div className={styles.container}>
+        <div className={styles.heading}>
+          <img src=""></img>
+        </div>
+      </div>
+      <div className={style.container1}>
+        <div className={style.left}>
+          <button>Wokers</button>
+          <button>Aanganwadi</button>
+          <button>Stock</button>
+          <button>Requests</button>
+          <button>Profile</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Header;
