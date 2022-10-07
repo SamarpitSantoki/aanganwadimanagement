2b6c0726aa3796a8d53ddf14048ed2eb5e01c9eb
shreyvegad <shreyvegad22@gmail.com>
2022-10-07 12:11:20 +0530

    Changes in Dropdowns

--- client/src/components/AanganwadiList/AanganwadiList.jsx
+++ client/src/components/AanganwadiList/AanganwadiList.jsx
@@ -124,11 +124,9 @@ const AanganwadiList
                                             variant={variant.toLowerCase()}
                                             title={variant}
                                         >
-                                            <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                                            <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                                            <Dropdown.Item eventKey="3">
-                                                Active Item
-                                            </Dropdown.Item>
+                                            <Dropdown.Item eventKey="1">Abc</Dropdown.Item>
+                                            <Dropdown.Item eventKey="2">Def</Dropdown.Item>
+                                            <Dropdown.Item eventKey="3">Abcd</Dropdown.Item>
                                         </DropdownButton>
                                     ),
                                 )}
