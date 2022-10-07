0a4215c807a5c3a52402bee8957f33028e4f66f5
dhruvrajsinh <dhruvsss007@gmail.com>
2022-10-07 11:52:49 +0530

    JWT

--- server/routes/login.route.js
+++ server/routes/login.route.js
@@ -1,7 +1,7 @@
 let express = require("express");
 const router = express.Router();
 const { Login, SignUp } = require("../controllers/login.controllers");
-
+const {protect} =require("../middleware/Auth");
 router.post("/login", Login);
 router.post("/singup", SignUp);
 
